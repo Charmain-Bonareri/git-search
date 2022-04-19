e5c76b6083bed6204290e95811e59e5ceb39cb6f
Charmain <mogerecharmain@gmail.com>
2022-04-19 17:02:35 +0300

    Generated user and repo service and created functions to get username and user repo.

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
-import { User } from './user';
+import { Repo } from './repo';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.prod';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
-  user: User;
+export class RepoService {
+  foundRepo: Repo;
+
 
   constructor(private http: HttpClient) {
-    this.user = new User('','', '', 0, 0, '','', new Date());
+    this.foundRepo = new Repo(0,0,'', new Date());
   }
-
-  async getUser(username: string) {
-    const searchUser = this.http.get<User>(
-      `${environment.apiurl}/users/${username}`
+  async getRepo(username: string) {
+    const searchUser = this.http.get<Repo>(
+      `${environment.apiurl}/users/${username}/repos`
     );
     return await lastValueFrom(searchUser).then((result) => {
       console.log(result);
-      return result
+      return result;
     });
   }
 }
